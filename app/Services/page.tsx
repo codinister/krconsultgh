6c775a65fb3d686d11460b2d8cc12605c7a5bcf7
codinister <codinister@gmail.com>
2025-11-15 16:43:41 +0100

    Fixed page display issue and changed hero image

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -2,8 +2,9 @@
 
 import Pageheader from '@/components/Pageheader';
 import Portfolio from '@/components/sections/Portfolio';
+import Services from '@/components/sections/Services';
 
-export default function Services() {
+export default function ServicesPage() {
   return (
     <>
       <Pageheader title="Our Services" />
